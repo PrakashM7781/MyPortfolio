0fe609d2085b55b120a21f6f022770b91df0ee80
Prakash Mishra <featplayerexclusive@gmail.com>
2023-07-17 23:21:41 +0530

    Add existing project files to Git

--- script/sript.js
+++
@@ -1,8 +0,0 @@
-const bars = document.querySelectorAll(".graph-bar");
-bars.forEach((bar) => {
-  const width = bar.style.width;
-  bar.style.width = "0";
-  setTimeout(() => {
-    bar.style.width = width;
-  }, 500);
-});
